d08c238c93a5f467da79e91c22627baf075afe7a
Quinton Woodhouse <woodhoqr@dukes.jmu.edu>
2018-02-16 13:19:51 -0500

    took away files

--- schema/alter.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE division ADD PRIMARY KEY (div_num);
-
-ALTER TABLE school ADD PRIMARY KEY (div_num, sch_num);
-ALTER TABLE school ADD FOREIGN KEY (div_num) REFERENCES division;
-
-ALTER TABLE college ADD PRIMARY KEY (unitid);
