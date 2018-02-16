9ec546990360e50514b9271ff43121c439d9ce1e
Jordan Domsky <domskyja@dukes.jmu.edu>
2018-02-16 16:40:33 -0500

    Removed extra files

--- alter.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE catalog ADD PRIMARY KEY (Prefix, Code);
-
-ALTER TABLE enrollment ADD PRIMARY KEY (subject, number, nbr);
-ALTER TABLE enrollment ADD FOREIGN KEY (subject, number) REFERENCES catalog (Prefix, Code);
--- stats.sql
+++
@@ -1,5 +0,0 @@
-ANALYZE VERBOSE catalog;
-SELECT count(*) AS cat_cnt FROM catalog;
-
-ANALYZE VERBOSE enrollment;
-SELECT count(*) AS enroll_cnt FROM enrollment;
