7a4d054f43bd0460a12f9d934e35e99e1fdf5ebe
Jordan Domsky <domskyja@dukes.jmu.edu>
2018-03-15 14:54:58 -0400

    Added two queries and the template for the chart

---
+++ queries/filled.sql
@@ -0,0 +1,24 @@
+﻿--
+-- Finds the times that this course has seats left open
+-- NOTE: we should probably change this to take in a major, number 
+--       that returns the number of seats left over for each class
+--       then could display a chart that shows the percentage of
+--       seats left over
+
+DROP FUNCTION IF EXISTS filled(subject text, number text);
+
+CREATE FUNCTION filled(subject text, number text)
+RETURNS TABLE(term integer, enrolled integer, enrl_cap integer) AS $$
+
+    SELECT term, enrolled, enrl_cap
+    FROM (
+        SELECT distinct term, nbr, subject, number, enrolled, enrl_cap
+        FROM enrollment
+        WHERE subject = $1
+	    AND number = $2) sub
+    WHERE enrolled < enrl_cap
+    ORDER BY term;
+
+$$ LANGUAGE SQL STABLE STRICT;
+
+ALTER FUNCTION filled(subject text, number text) OWNER TO seeds;
---
+++ queries/top_enrolled.sql
@@ -0,0 +1,21 @@
+﻿--
+-- Finds the top 10 most enrolled courses by given major
+--
+
+DROP FUNCTION IF EXISTS top_enrolled(subject text);
+
+CREATE FUNCTION top_enrolled(subject text)
+RETURNS TABLE(number text, sum bigint) AS $$
+
+    SELECT number, sum(enrolled)
+    FROM (
+        SELECT distinct term, nbr, subject, number, enrolled
+        FROM enrollment
+        WHERE subject = $1) sub
+    GROUP BY subject, number
+    ORDER BY sum DESC
+    LIMIT 10;
+
+$$ LANGUAGE SQL STABLE STRICT;
+
+ALTER FUNCTION top_enrolled(subject text) OWNER TO seeds;
