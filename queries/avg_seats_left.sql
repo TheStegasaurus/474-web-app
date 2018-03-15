18220398bdd813818f5e69c27b56008b3d1e983d
Jordan Domsky <domskyja@dukes.jmu.edu>
2018-03-15 15:13:16 -0400

    Updated example to use our data but the chart needs to be fixed

---
+++ queries/avg_seats_left.sql
@@ -0,0 +1,21 @@
+﻿--
+-- Finds the average number of seats left over in each class for given major
+-- NOTE: This can return negatives
+--
+
+DROP FUNCTION IF EXISTS avg_seats_left(subject text);
+
+CREATE FUNCTION avg_seats_left(subject text)
+RETURNS TABLE(number text, avg_left numeric) AS $$
+
+    SELECT number, avg(enrl_cap - enrolled) as avg_left
+    FROM (
+	    SELECT distinct term, nbr, subject, number, enrolled, enrl_cap
+	    FROM enrollment
+	    WHERE subject = $1
+	    AND enrl_cap > 0) sub
+    GROUP BY number
+
+$$ LANGUAGE SQL STABLE STRICT;
+
+ALTER FUNCTION avg_seats_left(subject text) OWNER TO seeds;
